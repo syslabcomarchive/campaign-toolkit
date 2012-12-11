344b4693ed92c4f9b8b4a6babc301f0ca3fb6c7a
Peter Lamut <peter.lamut@niteoweb.com>
2012-12-11 15:16:03 +0100

    Development buildout: use flake8 instead of pyflakes and pep8

--- pre-commit-check.sh
+++ pre-commit-check.sh
@@ -14,11 +14,8 @@ bin/test -s osha.campaigntoolkit; handle_exit
 echo '====== Running ZPTLint ======'
 for pt in `find src/osha.campaigntoolkit/ -name "*.pt"` ; do bin/zptlint $pt; done
 
-echo '====== Running PyFlakes ======'
-bin/pyflakes src/osha.campaigntoolkit/; handle_exit
-
-echo '====== Running pep8 =========='
-bin/pep8 --ignore=E501 src/osha.campaigntoolkit/; handle_exit
+echo '====== Running flake8 =========='
+bin/flake8 --ignore=E501 src/osha.campaigntoolkit/ --exclude=Products.CMFPlone.skins.plone_scripts.livesearch_reply.py; handle_exit
 
 if [ $EXITCODE -ne 0 ]; then
     exit 1
